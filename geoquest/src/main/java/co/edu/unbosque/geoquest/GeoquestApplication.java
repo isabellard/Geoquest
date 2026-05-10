364449bd9dea09f52b15da600af0d6e4bff355d0
Isabella Rodriguez <irodriguezme@unbosque.edu.co>
2026-05-10 11:09:47 -0500

    Add files via upload

---
+++ geoquest/src/main/java/co/edu/unbosque/geoquest/GeoquestApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.geoquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoquestApplication.class, args);
+	}
+
+}
---
+++ geoquest/src/test/java/co/edu/unbosque/geoquest/GeoquestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.geoquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
